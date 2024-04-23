67dc1bde049ae4790491a4b375c5888000d932fc
lixing <liaotiam@126.com>
2024-04-23 17:43:39 +0800

    first update

--- common/jwtx/jwt.go
+++ common/jwtx/jwt.go
@@ -2,11 +2,12 @@ package jwtx
 
 import "github.com/golang-jwt/jwt"
 
+// GetToken 密钥,现在时间,有效时间,用户id
 func GetToken(secretKey string, iat, seconds, uid int64) (string, error) {
 	claims := make(jwt.MapClaims)
 	claims["exp"] = iat + seconds
 	claims["iat"] = iat
-	claims["uid"] = uid
+	claims["uid"] = uid // 自定义参数
 	token := jwt.New(jwt.SigningMethodHS256)
 	token.Claims = claims
 	return token.SignedString([]byte(secretKey))
